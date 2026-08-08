a2074f0a4bd4d287d0b77aec8dd2f2c1c35d1713
Anand <anand@gounravel.com>
2026-08-08 18:21:33 +0200

    Added pdfs

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,9 @@ import boardspeakImage from '../assets/images/audioguide3.png';
 import bcombsImage from '../assets/images/dsp7.png';
 import sparlyimage from '../assets/images/sparly1.png';
 import nestaraImage from '../assets/images/villagetreasure3.png';
-import pockeyImage from '../assets/images/dsp4.png';
+import pockeyImage from '../assets/images/pockey1.png';
+import passImage from '../assets/images/pass.png';
+import livlytImage from '../assets/images/livlyt.png';
 
 
 export type Project = {
@@ -82,6 +84,29 @@ export const projects: Project[] = [
 		alt: 'Nestara mobile app preview showing real estate product screens',
 		imageClass: 'project-preview-image',
 	},
-	
+	{
+		name: '',
+		title: 'A treasure hunting adventuremobile game',
+		description:
+			'A mobile-first experience focused on reassurance, next-step clarity, and decision confidence.',
+		background: 'card4',
+		variant: 'mobile',
+		href: '/pdf/pass.pdf',
+		image: passImage,
+		alt: 'Nestara mobile app preview showing real estate product screens',
+		imageClass: 'project-preview-image',
+	},
+	{
+		name: '',
+		title: 'A treasure hunting adventuremobile game',
+		description:
+			'A mobile-first experience focused on reassurance, next-step clarity, and decision confidence.',
+		background: 'card4',
+		variant: 'mobile',
+		href: '/pdf/livlyt.pdf',
+		image: livlytImage,
+		alt: 'Nestara mobile app preview showing real estate product screens',
+		imageClass: 'project-preview-image',
+	},
 	
 ];
